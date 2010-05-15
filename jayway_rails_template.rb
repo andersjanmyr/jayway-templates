2e9a99e6a7f83242fbf1fa4dd3d886dae5f5cdb6
Anders Janmyr <anders.janmyr@jayway.com>
2010-05-15 12:02:24 +0200

    Updated to use more of the builtin actions of Thor.

--- jayway_rails_template.rb
+++ jayway_rails_template.rb
@@ -1,24 +1,13 @@
-# Setup git 
-run "touch db/.gitkeep lib/tasks/.gitkeep log/.gitkeep tmp/.gitkeep public/stylesheets/.gitkeep vendor/plugins/.gitkeep"
-
-append_file '.gitignore' , <<-EOT 
-.DS_Store
-.bundle
-.idea
-db/*.sqlite3
-log/*.log
-public/stylesheets/*.css
-tmp/**/*
-EOT
-
+# Application template for Jayway projects
 
 # Replace test framework
-run "rm -r test"
-# run "gem install rspec-rails --pre"
-gem "rspec", ">= 2.0.0.beta.8", :group => :test
-gem "rspec-rails", ">= 2.0.0.beta.8", :group => :test
-gem "factory_girl", :git => "git://github.com/szimek/factory_girl.git", :branch => "rails3", :group => :test
+remove_dir 'test'
+# run 'gem install rspec-rails --pre'
+gem 'rspec', '>= 2.0.0.beta.8', :group => :test
+gem 'rspec-rails', '>= 2.0.0.beta.8', :group => :test
+gem 'factory_girl', :git => 'git://github.com/szimek/factory_girl.git', :branch => 'rails3', :group => :test
 
+# Cucumber integration test
 gem 'capybara', :group => :test
 gem 'database_cleaner', :group => :test
 gem 'cucumber-rails', :group => :test
@@ -28,14 +17,14 @@ gem 'launchy', :group => :test
 gem 'cucumber', :group => :test
 
 # Install View
-gem "haml", "3.0.2"
+gem 'haml', '3.0.2'
 gem 'rails3-generators', :git => 'http://github.com/andersjanmyr/rails3-generators.git'
-gem "formtastic", :git => 'http://github.com/justinfrench/formtastic.git', :branch => 'rails3'
-run "curl -L http://github.com/justinfrench/formtastic/raw/master/generators/formtastic/templates/formtastic.rb > config/initializers/formtastic.rb"
-gem "compass"
+gem 'formtastic', :git => 'http://github.com/justinfrench/formtastic.git', :branch => 'rails3'
+get 'http://github.com/justinfrench/formtastic/raw/master/generators/formtastic/templates/formtastic.rb', 'config/initializers/formtastic.rb'
+gem 'compass'
 
-run "rm app/views/layouts/application.html.erb"
-file 'app/views/layouts/application.html.haml' , <<-EOT 
+remove_file 'app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.haml' , <<-HAML 
 !!!
 %html
   %head
@@ -44,41 +33,66 @@ file 'app/views/layouts/application.html.haml' , <<-EOT
     = stylesheet_link_tag 'print.css', :media => 'print'
     /[if lt IE 8]
       = stylesheet_link_tag 'ie.css', :media => 'screen, projection'
-    = javascript_include_tag 'jquery', 'rails'
+    = javascript_include_tag :jquery
     = csrf_meta_tag
   %body
     = yield
-EOT
+HAML
 
 
-# Configure Rails
-config_text = <<-EOT 
+# Replace prototype with jQuery.
+initializer 'jquery.rb', <<-JQUERY
+if Rails.env.development?
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion \
+  :jquery => %w(jquery jquery-ui rails)
+else
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion \
+  :jquery => %w(jquery.min jquery-ui.min rails)
+end    
+JQUERY
+
+remove_file 'public/javascripts/controls.js'
+remove_file 'public/javascripts/dragdrop.js'
+remove_file 'public/javascripts/effects.js'
+remove_file 'public/javascripts/prototype.js'
+remove_file 'public/javascripts/rails.js'
+get 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js', 'public/javascripts/jquery.js'
+get 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'public/javascripts/jquery.min.js'
+get 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.js', 'public/javascripts/jquery-ui.js'
+get 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js', 'public/javascripts/jquery-ui.min.js'
+get 'http://github.com/rails/jquery-ujs/raw/master/src/rails.js', 'public/javascripts/rails.js'
+
+# Configure Rails Generators
+application <<-GENERATORS
     config.generators do |g| 
       g.orm  :active_record  
       g.template_engine :formtastic_haml  
-      g.test_framework  :rspec, :fixture => true 
-      g.integration_tool  :rspec
+      g.test_framework :rspec, :fixture => true, :views => false, :view_specs => false 
+      g.integration_tool :cucumber
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.stylesheets false
     end 
-EOT
-inject_into_file  "config/application.rb", config_text, :after => "class Application < Rails::Application\n"
-
-# Replace prototype with jQuery.
-run "rm public/javascripts/controls.js"
-run "rm public/javascripts/dragdrop.js"
-run "rm public/javascripts/effects.js"
-run "rm public/javascripts/prototype.js"
-run "curl -L http://code.jquery.com/jquery-1.4.2.js > public/javascripts/jquery.js"
-run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
+GENERATORS
 
 # Run the generators
-run "bundle install"  
+run 'bundle install'  
 generate 'rspec:install'
 generate 'cucumber:skeleton', 'rspec', 'capybara'
-run "compass create --app rails --sass-dir app/stylesheets --css-dir public/stylesheets ."
+run 'compass create --app rails --sass-dir app/stylesheets --css-dir public/stylesheets .'
 
 # Git
+run 'touch db/.gitkeep lib/tasks/.gitkeep log/.gitkeep tmp/.gitkeep public/stylesheets/.gitkeep vendor/plugins/.gitkeep'
+
+append_file '.gitignore' , <<-GIT 
+.DS_Store
+.bundle
+.idea
+db/*.sqlite3
+log/*.log
+public/stylesheets/*.css
+tmp/**/*
+GIT
+
 git :init
-git :add => "."
-git :commit => "-a -m 'Initial commit'"
+git :add => '.'
+git :commit => '-a -m "Initial commit"'
