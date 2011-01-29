64bb044662c42c8509c45ca0ac6a763cd70ba74f
Anders Janmyr <anders@janmyr.com>
2011-01-29 12:27:59 +0100

    moved spec_helper.rb

--- lib/templates/rails_template.rb
+++ lib/templates/rails_template.rb
@@ -37,7 +37,10 @@ ActionView::Helpers::AssetTagHelper.register_javascript_expansion \
   :jquery => %w(jquery jquery-ui rails)
 else
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion \
-  :jquery => %w(jquery.min jquery-ui.min rails)
+  :jquery => 
+  %w(http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js
+   http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.8/jquery-ui.min.js   
+   rails)
 end
 JQUERY
 
@@ -47,9 +50,7 @@ remove_file 'public/javascripts/effects.js'
 remove_file 'public/javascripts/prototype.js'
 remove_file 'public/javascripts/rails.js'
 get 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js', 'public/javascripts/jquery.js'
-get 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js', 'public/javascripts/jquery.min.js'
-get 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.js', 'public/javascripts/jquery-ui.js'
-get 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js', 'public/javascripts/jquery-ui.min.js'
+get 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.8/jquery-ui.js', 'public/javascripts/jquery-ui.js'
 get 'https://github.com/rails/jquery-ujs/raw/master/src/rails.js', 'public/javascripts/rails.js'
 
 # Configure Rails Generators
@@ -68,7 +69,7 @@ application <<-GENERATORS
   end
 GENERATORS
 
-get 'https://github.com/andersjanmyr/jayway-templates/raw/master/lib/templates/rails'
+`wget --no-check-certificate https://github.com/andersjanmyr/jayway-templates/raw/master/lib/templates/rails`
 
 run "gem install bundler"
 run "bundle install"
@@ -80,6 +81,8 @@ remove_dir 'test'
 run "rails g responders:install"
 run "rails g simple_form:install"
 run "rails g rspec:install"
+
+get 'https://github.com/andersjanmyr/jayway-templates/raw/master/lib/templates/rails/spec/spec_helper.rb', 'spec_helper.rb'
 
 initializer 'sass.rb', <<-SASS
 Sass::Plugin.options[:template_location] = './app/stylesheets'
